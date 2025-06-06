0ede60814ca2f465189350ed5949071d88609ad5
Siu Hang Wong <siuhangwong@hotmail.com>
2025-06-06 18:59:03 +0100

    New project: M2030 design system

--- js/project-page.js
+++ js/project-page.js
@@ -1,12 +1,5 @@
 
 /* Add class to side nav when scrolling past div with id*/
-
-$('.sidebar a').on('click', function(event) {
-    $(this).parent().find('a').removeClass('active');
-    $(this).addClass('active');
-});
-
-
 $(window).on("scroll", function() {
     $(".target").each(function() {
         if($(window).scrollTop() >= $(this).offset().top - 250) {
